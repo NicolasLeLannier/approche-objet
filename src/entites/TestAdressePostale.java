511728e654f5a0ecb7118049d2b9608833b6541c
NicolasLeLannier <lelannier.nicolas@gmail.com>
2023-10-06 12:02:07 +0200

    v3 : Fin TP 2

--- src/entites/TestAdressePostale.java
+++ src/entites/TestAdressePostale.java
@@ -5,18 +5,20 @@ public class TestAdressePostale {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		AdressePostale adr1 = new AdressePostale();
-		AdressePostale adr2 = new AdressePostale();
+		AdressePostale adr1 = new AdressePostale(37390, "Nana", 6, "Tours");
+		AdressePostale adr2 = new AdressePostale(89390, "Jazejl", 4, "Orléans");
 		
-		adr1.codePostal = 37390;
-		adr1.libelleRue = "Nana";
-		adr1.numeroRue = 6;
-		adr1.ville = "Tours";
+//		adr1.codePostal = 37390;
+//		adr1.libelleRue = "Nana";
+//		adr1.numeroRue = 6;
+//		adr1.ville = "Tours";
+//		
+//		adr2.codePostal = 89390;
+//		adr2.libelleRue = "Jazejl";
+//		adr2.numeroRue = 4;
+//		adr2.ville = "Orléans";
 		
-		adr2.codePostal = 89390;
-		adr2.libelleRue = "Jazejl";
-		adr2.numeroRue = 4;
-		adr2.ville = "Orléans";
+		System.out.println(adr1.ville);
 	}
 
 }
--- src/entites/TestPersonne.java
+++ src/entites/TestPersonne.java
@@ -7,29 +7,28 @@ public class TestPersonne {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Personne p1 = new Personne();
-		Personne p2 = new Personne();
+		Personne p1 = new Personne("Titiname", "Titi");
+		Personne p2 = new Personne("Le Lannier", "Laura");
 		
-		AdressePostale adr1 = new AdressePostale();
-		AdressePostale adr2 = new AdressePostale();
+		AdressePostale adr1 = new AdressePostale(37390, "Nana", 6, "Tours");
+		AdressePostale adr2 = new AdressePostale(89390, "Jazejl", 4, "Orléans");
 		
-		adr1.codePostal = 37390;
-		adr1.libelleRue = "Nana";
-		adr1.numeroRue = 6;
-		adr1.ville = "Tours";
-		
-		adr2.codePostal = 89390;
-		adr2.libelleRue = "Jazejl";
-		adr2.numeroRue = 4;
-		adr2.ville = "Orléans";
-		
-		p1.nom = "Titiname";
-		p1.prenom = "Titi";
+//		p1.nom = "Titiname";
+//		p1.prenom = "Titi";
 		p1.adresse = adr1;
 		
-		p2.nom = "Le Lannier";
-		p2.prenom = "Laura";
+//		p2.nom = "Le Lannier";
+//		p2.prenom = "Laura";
 		p2.adresse = adr2;
+		
+		
+		Personne p3 = new Personne("Titiname", "Titi", adr1);
+		Personne p4 = new Personne("Le Lannier", "Laura", adr2);
+		
+		System.out.println(p4.nom);
+		System.out.println(p3.prenom);
+		System.out.println(p1.prenom);
+		System.out.println(p3.nom + "  " +p3.prenom + " :  " + p3.adresse.ville);
 	}
 
 }
